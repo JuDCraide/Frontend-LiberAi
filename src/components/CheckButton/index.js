cdad5518c908ef9c89e16b9592844b802144d2bd
Andressa <andressa.alves@syonet.com>
2020-09-02 13:29:02 -0300

    feat(components): add check button

---
+++ src/components/CheckButton/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CheckButtonStyle from "./style";
+
+function CheckButton() {
+  return (
+    <CheckButtonStyle>
+      <i class="material-icons">check_circle</i>
+    </CheckButtonStyle>
+  );
+}
+
+export default CheckButton;
