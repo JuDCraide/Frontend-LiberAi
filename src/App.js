d00b8823be78c5c533198f3a7d3ba845d4f0e343
Andressa <andressa.alves@syonet.com>
2020-09-02 13:33:51 -0300

    feat(style): add App.css, styled-components and materialize

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import "./App.css";
+import CheckButton from "./components/CheckButton";
 
 function App() {
   return (
     <div>
       <h1>Hello Liberaí</h1>
+      <CheckButton />
     </div>
   );
 }
