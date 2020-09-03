40791daa9bc53baa2eb7b1f6465461cd4f4591d9
Andressa <andressa.alves@syonet.com>
2020-09-03 08:41:04 -0300

    feat(component): add BigContainer

---
+++ src/components/BigContainer/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const CoursesContainer = styled.div`
+  width: 45rem;
+  height: 27.7rem;
+
+  display: flex;
+  flex-direction: row;
+
+  padding: 1.5rem 1.375rem;
+`;
+
+const MainContainer = styled.div`
+  width: 48rem;
+  height: 14.5rem;
+
+  display: flex;
+  flex-direction: column;
+
+  padding: 1.5rem;
+`;
+
+export { CoursesContainer, MainContainer };
