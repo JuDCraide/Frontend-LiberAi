1cdc54af70b793c87639179cc6a5ccd2131196dd
JúDCraide <juliadcraide31@gmail.com>
2020-09-06 02:35:39 -0300

    feat(components): add ProfileMenu

---
+++ src/components/UserInfo/ProfileMenu/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const MenuStyle = styled.div`
+  display: flex;
+  flex-direction:column;
+  justify-content:center;
+  padding-left:10px
+`;
+
+
+export { MenuStyle };
